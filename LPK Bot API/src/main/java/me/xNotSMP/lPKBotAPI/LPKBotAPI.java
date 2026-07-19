58ae8bdd27c354aad42951c6fbcab5194bceec97
xXSquirtz <140819674+xXSquirtz@users.noreply.github.com>
2026-07-19 23:12:25 +0200

    Add files via upload

---
+++ LPK Bot API/src/main/java/me/xNotSMP/lPKBotAPI/LPKBotAPI.java
@@ -0,0 +1,16 @@
+package me.xNotSMP.lPKBotAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LPKBotAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
